f19026f4900199fccb396d23b2b6833ef8e2e0c1
Maxim <maxim.p11@gso.schule.koeln>
2025-05-07 11:01:37 +0200

    Würfelbecher Klasse

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -1,8 +1,34 @@
 ﻿using System;
 
-namespace Wuerfelspiel
+public class Wuerfel
 {
-    public class Wuerfel
+    private int seiten;
+    private int augenzahl;
+    private Random random;
+
+    public Wuerfel(int seiten)
     {
+        if (seiten < 1)
+            throw new ArgumentException("Ein Würfel muss mindestens eine Seite haben.");
+
+        this.seiten = seiten;
+        random = new Random();
+        Wuerfeln();
+    }
+
+    public int Seiten
+    {
+        get { return seiten; }
+    }
+
+    public int Augenzahl
+    {
+        get { return augenzahl; }
+    }
+
+    public int Wuerfeln()
+    {
+        augenzahl = random.Next(1, seiten + 1);
+        return augenzahl;
     }
 }
--- Wuerfelspiel/Wuerfelbecher.cs
+++ Wuerfelspiel/Wuerfelbecher.cs
@@ -1,13 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Wuerfelspiel
+public class Wuerfelbecher
 {
-    public class Wuerfelbecher
-    {
+    private List<Wuerfel> wuerfelListe;
+    private List<bool> festgehalteneWuerfelListe;
 
+    public Wuerfelbecher(int wuerfelAnzahl)
+    {
+        if (wuerfelAnzahl <= 0)
+            throw new ArgumentException("z+Zurzeit existieren keine Würfel. Es muss mindestens 1 Würfel geben!!!.");
+
+        wuerfelListe = new List<Wuerfel>();
+        festgehalteneWuerfelListe = new List<bool>();
+
+        for (int i = 0; i < wuerfelAnzahl; i++)
+        {
+            wuerfelListe.Add(new Wuerfel(6));
+            festgehalteneWuerfelListe.Add(false);
+        }
+    }
+
+    public void Wuerfeln()
+    {
+        for (int i = 0; i < wuerfelListe.Count; i++)
+        {
+            if (!festgehalteneWuerfelListe[i])
+                wuerfelListe[i].Wuerfeln();
+        }
+    }
+
+    public void Wuerfelfesthalten(int index, bool festhalten)
+    {
+        if(index < 0 || index >= festgehalteneWuerfelListe.Count)
+            throw new ArgumentException(nameof(index), "ungültiger Würfelindex");
+
+        festgehalteneWuerfelListe[index] = festhalten;
+    }
+    public List<int> Augenzahl
+    {
+        get
+        {
+            List<int> augen = new List<int>();
+            foreach(var w in wuerfelListe)
+                augen.Add(w.Augenzahl); 
+            return augen ;
+        }
     }
 }
+
